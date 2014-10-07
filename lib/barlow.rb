1a4accc81dc7e57930a09f671bddabdf36a331d3
Andrew Smith <andrewchristophersmith@gmail.com>
2014-10-07 15:43:39 -0700

    Began working with a bin file

--- lib/barlow.rb
+++ lib/barlow.rb
@@ -1,3 +1,27 @@
+require 'prime'
+
 class Barlow
   VERSION = "1.0.0"
+  def self.harmonicity num, den
+    sign = self.indigestibility(num) > self.indigestibility(den) ? 1.0 : -1.0
+    sign / (self.indigestibility(num) + self.indigestibility(den) - 2 * self.indigestibility(num.lcm(den)))
+  end
+
+  def self.indigestibility p
+    factors = p.prime_division
+    2.0 * factors.inject(0) {|memo, x| memo += ( x[1] * ( (x[0]-1)**2.0 ) / x[0] ) }
+  end
+  
+  # Returns the first differential ratio, reduced
+  def self.interior_ratio r1, r2
+    ratio = [r1[0] * r2[1], r1[1] * r2[0]]
+    gcd = ratio.first.gcd ratio.last
+    ratio.collect! {|r| r / gcd}
+  end
+
+  # Returns the harmonicity between the two ratio
+  def self.interior_harmonicity r1, r2
+    Barlow.harmonicity(*self.interior_ratio(r1, r2))
+  end
 end
+
--- test/test_barlow.rb
+++ test/test_barlow.rb
@@ -2,7 +2,12 @@ require "minitest/autorun"
 require "barlow"
 
 class TestBarlow < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_harmonicity
+    assert_in_delta 0.0717, Barlow.harmonicity(6, 7)
+  end
+
+  def test_indigestibility
+    assert_in_delta 5.3333, Barlow.indigestibility(9)
   end
 end
+
